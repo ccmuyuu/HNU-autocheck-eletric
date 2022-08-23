db3c11b2ae63e3092817e1f31080ee08313d7040
ccmuyuu <105057037+ccmuyuu@users.noreply.github.com>
2022-08-23 14:42:41 +0800

    Update setsmtp.sh

--- setsmtp.sh
+++ setsmtp.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+#不用qq邮箱自己将"smtp.qq.com:465"换成你用的邮箱的smtp域名及端口
 cp ssmtp.conf.bak ssmtp.conf
 echo "root=$mail_username" >> ssmtp.conf
-echo "mailhub=smtp.qq.com:465" >> ssmtp.conf
+echo "mailhub=smtp.qq.com:465" >> ssmtp.conf 
 echo "AuthUser=$mail_username" >> ssmtp.conf
 echo "AuthPass=$mail_password" >> ssmtp.conf
 echo "UseTls=Yes" >> ssmtp.conf
