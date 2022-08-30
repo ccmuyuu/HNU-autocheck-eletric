07cba5441e9ceb14b4534f764d8da79b4de247f8
ccmuyuu <105057037+ccmuyuu@users.noreply.github.com>
2022-08-30 13:02:03 +0800

    Update cqhttp.sh

--- cqhttp.sh
+++ cqhttp.sh
@@ -2,12 +2,12 @@
 . ./.config
 . ./data.config
 #若私聊请将send_group_msg改成send_private_msg,group_id改成user_id
-if [ "$daily_post" = "false" ] && [ `expr $remind_threshold \>= ${balance%???}` -eq 0 ] #我也不知道这里为什么长这样...
+if [ "$daily_post" = "false" ] && [ `echo "$remind_threshold >= ${balance%???}" | bc` -eq 1 ] 
 then 
-  curl "http://127.0.0.1:7654/send_group_msg" --data-urlencode 'group_id=12345678' --data-urlencode 'message='$park$building$RoomNo'电量仅剩$balance，请及时充值' 
+  curl "http://127.0.0.1:7654/send_group_msg" --data-urlencode 'group_id=12345678' --data-urlencode 'message='$park$building$RoomNo'电量仅剩'$balance'，请及时充值' 
 elif [ "$daily_post" = "true" ]
 then
-  curl "http://127.0.0.1:7654/send_group_msg" --data-urlencode 'group_id12345678' --data-urlencode 'message='$park$building$RoomNo'今日剩余电量为'$balance
+  curl "http://127.0.0.1:7654/send_group_msg" --data-urlencode 'group_id=12345678' --data-urlencode 'message='$park$building$RoomNo'今日剩余电量为'$balance
 else
   echo 0
 fi
