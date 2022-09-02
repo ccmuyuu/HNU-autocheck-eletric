1985cf4904032c4f1f1e1351880fed46de4dad3b
ccmuyuu <105057037+ccmuyuu@users.noreply.github.com>
2022-09-02 12:28:18 +0800

    Update tg.sh

--- tg.sh
+++ tg.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 . ./.config
 . ./data.config
-if [ "$daily_post" = "false" ] && [ `echo "$remind_threshold >= ${balance%???}" | bc` -eq 1 ]
-then 
-  curl -s -X POST "https://api.telegram.org/bot${bot_token}/sendMessage" -d chat_id=${chat_id} -d text="$park$building$RoomNo电量仅剩$balance，请及时充值"
-elif [ "$daily_post" = "true" ]
+if [ "$balance" != "null" ]
 then
-  curl -s -X POST "https://api.telegram.org/bot${bot_token}/sendMessage" -d chat_id=${chat_id} -d text="$park$building$RoomNo今日剩余电量为$balance"
+  if [ "$daily_post" = "false" ] && [ `echo "$remind_threshold >= ${balance%???}" | bc` -eq 1 ]
+  then 
+    curl -s -X POST "https://api.telegram.org/bot${bot_token}/sendMessage" -d chat_id=${chat_id} -d text="$park$building$RoomNo电量仅剩$balance，请及时充值"
+  elif [ "$daily_post" = "true" ]
+  then
+    curl -s -X POST "https://api.telegram.org/bot${bot_token}/sendMessage" -d chat_id=${chat_id} -d text="$park$building$RoomNo今日剩余电量为$balance"
+  else
+    echo 0
+  fi
 else
-  echo 0
+  echo null
 fi
