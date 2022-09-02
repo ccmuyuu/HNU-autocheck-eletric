072be2eb2c5a6b19a36900204fc9cde2cd171ae3
ccmuyuu <105057037+ccmuyuu@users.noreply.github.com>
2022-09-02 12:27:30 +0800

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 . ./.config
 . ./data.config
-if [ "$daily_post" = "false" ] && [ `echo "$remind_threshold >= ${balance%???}" | bc` -eq 1 ]
-then 
-    echo "$park$building$RoomNo电量仅剩$balance，请及时充值" | mail -s "title" $mail_add
-elif [ "$daily_post" = "true" ]
+if [ "$balance" != "null" ]
 then
-   echo "$park$building$RoomNo今日剩余电量为$balance" | mail -s "title" $mail_add
-else
-  echo 0
+  if [ "$daily_post" = "false" ] && [ `echo "$remind_threshold >= ${balance%???}" | bc` -eq 1 ]
+  then 
+      echo "$park$building$RoomNo电量仅剩$balance，请及时充值" | mail -s "title" $mail_add
+  elif [ "$daily_post" = "true" ]
+  then
+    echo "$park$building$RoomNo今日剩余电量为$balance" | mail -s "title" $mail_add
+  else
+    echo 0
+  fi
+else 
+  echo null
 fi
