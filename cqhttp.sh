7259a92ca9eba1986ec2e0e190f66e914db53408
ccmuyuu <105057037+ccmuyuu@users.noreply.github.com>
2022-09-02 12:26:28 +0800

    Update cqhttp.sh

--- cqhttp.sh
+++ cqhttp.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 . ./.config
 . ./data.config
-#若私聊请将send_group_msg改成send_private_msg,group_id改成user_id
-if [ "$daily_post" = "false" ] && [ `echo "$remind_threshold >= ${balance%???}" | bc` -eq 1 ] 
-then 
-  curl "http://127.0.0.1:7654/send_group_msg" --data-urlencode 'group_id=12345678' --data-urlencode 'message='$park$building$RoomNo'电量仅剩'$balance'，请及时充值' 
-elif [ "$daily_post" = "true" ]
+if [ "$balance" != "null" ]
 then
-  curl "http://127.0.0.1:7654/send_group_msg" --data-urlencode 'group_id=12345678' --data-urlencode 'message='$park$building$RoomNo'今日剩余电量为'$balance
+#若私聊请将send_group_msg改成send_private_msg,group_id改成user_id
+  if [ "$daily_post" = "false" ] && [ `echo "$remind_threshold >= ${balance%???}" | bc` -eq 1 ] 
+  then 
+    curl "http://127.0.0.1:7654/send_group_msg" --data-urlencode 'group_id=12345678' --data-urlencode 'message='$park$building$RoomNo'电量仅剩'$balance'，请及时充值' 
+  elif [ "$daily_post" = "true" ]
+  then
+    curl "http://127.0.0.1:7654/send_group_msg" --data-urlencode 'group_id=12345678' --data-urlencode 'message='$park$building$RoomNo'今日剩余电量为'$balance
+  else
+    echo 0
+  fi
 else
-  echo 0
+  echo null
 fi
